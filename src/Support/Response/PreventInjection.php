0f5eefc79cbab1f0eb1149327064ef26d9505ca2
AJ Meireles <alvaro.meireles@live.com>
2024-09-15 17:03:18 -0300

    wip

--- src/Support/Response/PreventInjection.php
+++ src/Support/Response/PreventInjection.php
@@ -19,7 +19,7 @@ readonly class PreventInjection
      */
     public function aborted(): bool
     {
-        if (! config('envbar.enabled')) {
+        if (! config('envbar.enabled') || app()->runningUnitTests()) {
             return true;
         }
 
@@ -42,12 +42,18 @@ readonly class PreventInjection
         return false;
     }
 
+    /**
+     * Abort the injection according to the route rule.
+     */
     private function forRoutes(): bool
     {
         return collect(config('envbar.ignore_on'))
             ->contains(fn (string $route) => $this->request->is($route));
     }
 
+    /**
+     * Abort the injection according to the authenticated user rule.
+     */
     private function forAuthenticatedUsers(): bool
     {
         $fluent = new Fluent(config('envbar.for_authenticated_users'));
@@ -59,11 +65,17 @@ readonly class PreventInjection
         return ! Gate::allows('envbar::view', $this->request->user($fluent->get('guard')));
     }
 
+    /**
+     * Abort the injection according to the environment rule.
+     */
     private function forEnvironments(): bool
     {
         return ! in_array(app()->environment(), array_keys(config('envbar.environments')));
     }
 
+    /**
+     * Abort the injection according to the mobile rule.
+     */
     private function forMobile(): bool
     {
         if (config('envbar.on_mobile')) {
