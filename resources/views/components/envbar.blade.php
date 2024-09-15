bc08ab99a3cdb3557679a0a95b52d51937115e58
AJ Meireles <alvaro.meireles@live.com>
2024-09-15 16:53:52 -0300

    wip

--- resources/views/components/envbar.blade.php
+++ resources/views/components/envbar.blade.php
@@ -4,27 +4,31 @@
         'eb-sticky' => $configuration['fixed'],
         $configuration['colors']
     ]) x-data="envbar()" x-show="show">
-    <div class="eb-flex eb-items-center eb-ml-2 eb-gap-1">
+    <div class="eb-flex eb-flex-wrap eb-items-center eb-ml-2 eb-gap-1">
+        {{-- Environment --}}
         <div class="eb-inline-flex eb-items-center eb-gap-1">
             <x-envbar::icons.laravel class="eb-h-6 eb-w-6 eb-text-green-700" />
-            @lang('envbar::messages.environment')
+            <p>@lang('envbar::messages.environment')</p>
             <x-envbar::badge>
-                local
+                Local
             </x-envbar::badge>
             <x-envbar::icons.fork class="eb-h-6 eb-w-6 eb-text-green-700" />
         </div>
+        {{-- Branch --}}
         <div class="eb-flex eb-flex-row eb-items-center eb-gap-1">
-            <div class="eb-inline-flex eb-items-center eb-gap-1">
-                @lang('envbar::messages.branch')
+            <div class="eb-inline-flex eb-items-center">
+                <p>@lang('envbar::messages.branch')</p>
                 <x-envbar::badge>feat/foobar</x-envbar::badge>
             </div>
         </div>
+        {{-- Release --}}
         <div class="eb-inline-flex eb-items-center eb-gap-1">
             <x-envbar::icons.tag class="eb-h-4 eb-w-4 eb-text-green-700" />
-            @lang('envbar::messages.release')
+            <p>@lang('envbar::messages.release')</p>
             <x-envbar::badge>v2.11.23</x-envbar::badge>
         </div>
-        <div class="eb-flex eb-absolute eb-right-1 eb-items-center">
+        {{-- Right Side --}}
+        <div class="eb-flex eb-items-center eb-absolute eb-right-2">
             <x-envbar::badge>XL</x-envbar::badge>
             @if ($configuration['closable']['enabled'])
                 <button type="button" x-on:click="close(@js($configuration['closable']['timeout']))">
