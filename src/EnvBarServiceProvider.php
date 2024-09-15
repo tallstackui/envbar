3e4a5819df4812e182185f02273f767dd3df2b3a
AJ Meireles <alvaro.meireles@live.com>
2024-09-15 01:26:46 -0300

    wip

--- src/EnvBarServiceProvider.php
+++ src/EnvBarServiceProvider.php
@@ -10,20 +10,12 @@ class EnvBarServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->registerConfiguration();
-
-        $this->registerMiddleware();
-    }
-
-    private function registerConfiguration(): void
-    {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'envbar');
-        $this->mergeConfigFrom(__DIR__.'/../config/envbar.php', 'envbar');
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'envbar');
-    }
 
-    private function registerMiddleware(): void
-    {
+        $this->mergeConfigFrom(__DIR__.'/../config/envbar.php', 'envbar');
+
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'envbar');
+
         $this->app[Kernel::class]->appendMiddlewareToGroup('web', Injection::class);
     }
 }
