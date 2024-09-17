beaf42885bfc01fa5cc5d205d3aee6ae1d6a2dcd
AJ Meireles <alvaro.meireles@live.com>
2024-09-16 23:36:16 -0300

    fixing injection middleware with array_map and explode, implode

--- src/Response/ResponseHandle.php
+++ src/Response/ResponseHandle.php
@@ -19,12 +19,22 @@ class ResponseHandle
         $content = $this->response->getContent();
 
         if (($head = strpos($content, '<head>')) !== false) {
-            $content = substr_replace($content, $this->render->css(), $head + 6, 0);
-            $content = substr_replace($content, $this->render->js(), $head + 6, 0);
+            $content = substr_replace($content, $this->render->css(), $head + 6, 0); // @phpstan-ignore-line
+            $content = substr_replace($content, $this->render->js(), $head + 6, 0); // @phpstan-ignore-line
         }
 
-        $content = preg_replace('/(<body[^>]*>)/i', '$1'.$this->render->component(), $content);
+        // Although this code is not the best, it was the only viable way found, at least for now,
+        // to insert the envbar in the correct place, after <body>, for situations where we have things
+        // like "x-on:redirect="setTimeout(() => window.location.href = $event.detail.route, $event.detail.time ?? 3000)">"
+        // inside the <body>, things with symbols that mess up regex.
+        $explode = array_map(function (string $line): string {
+            if (str_contains($line, '<body')) {
+                return $line.$this->render->component(); // @phpstan-ignore-line
+            }
 
-        return $this->response->setContent($content);
+            return $line;
+        }, explode("\n", $content));
+
+        return $this->response->setContent(implode("\n", $explode));
     }
 }
